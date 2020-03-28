70e1493b2b0d9d677b0613e4d0418d75f1d1ea4b
Harsh Parmar <51324762+harshyp47@users.noreply.github.com>
2020-03-28 20:15:26 +0530

    Add files via upload

---
+++ wavesort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"WAVE SORT"<<endl;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements in space separated way:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i+=2){
+
+        //LEFT SIDE SWAPPING
+        if(arr[i]>arr[i-1] && i!=0){
+            swap(arr[i],arr[i-1]);
+        }
+
+        //RIGHT SIDE SWAPPING
+        if(arr[i]>arr[i+1] && i!=n-1){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+
+    //printing the array
+    cout<<endl;
+    cout<<"Your Wave Sort answer is:";
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
